877de028a9e4f06420c4d6f098318dc1ebff086c
will <pilapires@gmail.com>
2025-02-26 10:53:46 -0300

    Version 0.0.19 Correção de formatação

--- Shopjm/jm-shop-main/react/components/ProductTags/index.jsx
+++ Shopjm/jm-shop-main/react/components/ProductTags/index.jsx
@@ -13,15 +13,20 @@ const DiscountedPrice = () => {
 
   const sellingPrice = seller.commertialOffer.Price
   const discountedPrice = sellingPrice * 0.85 // Aplicando 10% de desconto
-  const formattedPrice = `R$ ${discountedPrice.toFixed(2).replace(".", ",")}`
-
+  const formattedPrice = `R$ ${discountedPrice
+    .toFixed(2) // Define duas casas decimais
+    .replace(".", ",") // Troca ponto por vírgula nos centavos
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ".")}` // Adiciona ponto nos milhares
+  
   return (
     <div className="discounted-price-container">
-      
       <span className={styles.discountedPrice}>{formattedPrice}</span>
-      <span className={styles.discountedPriceText}> <br></br> à vista cartão ou pix(15% off)</span>
+      <span className={styles.discountedPriceText}>
+        <br /> à vista, no cartão ou Pix (15% off)
+      </span>
     </div>
   )
+  
 }
 
 export default DiscountedPrice
